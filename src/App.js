fae3245daf089aba95763ad9485cafee1b1c7580
Immanuel Grönlund <immanuel@LAPTOP-J1RM6EGM.localdomain>
2022-10-19 14:51:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This App is now modified again 
         </p>
+        <p>Back again with docker-compose !</p>
         <a
           className="App-link"
           href="https://reactjs.org"
